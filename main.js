c3471f97554844cdc3f30f428e2fdacf50cad6ba
yashwant-c <74008172+yashwant-c@users.noreply.github.com>
2021-05-30 19:29:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run (event) {
+    console.log (event);
+
+    var Content = event.results[0][0].transcript;
+    console.log(event);
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+      speak();
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
